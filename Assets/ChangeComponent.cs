c24b3bfebbc8d5041617aa4be05704a5b3117859
RezaSitorus0602 <101858489+RezaSitorus0602@users.noreply.github.com>
2023-04-13 21:46:49 +0700

    first commit

---
+++ Assets/ChangeComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;   
+    }
+}
